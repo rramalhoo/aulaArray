b28b0d23cc37c8eba8269d355726a6a6fb5e2c2e
rramalhoo <raphael.athenesi@gmail.com>
2024-09-20 10:42:24 -0300

    Primeira aula de Array com o prof Alex

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const carros = ['Renegade', 'Toro', 'Tracker'];
+
+
+//mesma coisa q o de cima por questões visuais da enter
+const carrosClassicos = [
+    'Fusca',
+    'Corcel',
+    'Chevette'
+];
+
+const carrosEsportivos = [];
+carrosEsportivos[0] = 'Ferrari';
+carrosEsportivos[1] = 'McLaren';
+carrosEsportivos[2] = 'Porsche';
+
+const carrosUtilitarios = new Array('Montanda', 'Toro', 'S10');
+
+carrosClassicos[3] = 'Opala';
